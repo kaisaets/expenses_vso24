e23b5ad4600088a1392e996be1b6f7b20d592a70
Kaisa Ets <kaisa.ets@voco.ee>
2024-11-22 11:35:41 +0200

    typo fix

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -7,9 +7,10 @@ const ExpenseItem = (props) => {
     return (
     <div class='expense-item'>
         <ExpenseDate date={props.data.date} /> 
-        <div className='expense-item_description'></div>
+        <div className='expense-item_description'>
         <h2>{props.data.title}</h2>
         <div className = 'expense-item__price'>{props.data.price}</div>
+    </div>
     </div>
     )
 }
