3c45770a34b242c855179470887f9b7641f1e165
Kaisa Ets <kaisa.ets@voco.ee>
2024-11-22 11:32:59 +0200

    Divide component to ExpenseItem and ExpenseDate

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,16 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
+
 const ExpenseItem = (props) => {
+    
 
-
-    return (<div class='expense-item'>
-        <div>{props.data.date.toString()}</div>
-        <div className = 'expense-item__description'>
+    return (
+    <div class='expense-item'>
+        <ExpenseDate date={props.data.date} /> 
+        <div className='expense-item_description'></div>
         <h2>{props.data.title}</h2>
-        </div>
         <div className = 'expense-item__price'>{props.data.price}</div>
-    </div>)
+    </div>
+    )
 }
 export default ExpenseItem
