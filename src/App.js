3a415865d391852b784565edfaa8a3bcaed2c19b
Kaisa Ets <kaisa.ets@voco.ee>
2024-11-22 11:14:38 +0200

    yl 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
-function App() {
-  return (
-    <div className="App">
-      <ExpenseItem />
-      <ExpenseItem />
-    </div>
-  );
+const App = () =>{
+  const expenses =[
+
+    {
+      date: new Date (2024, 10, 22),
+      title: 'New book',
+      price: 39.99
+    } ,
+    {
+      date: new Date (2024, 10, 22),
+      title: 'New bag',
+      price: 99.99
+    } 
+  ] 
+
+return (
+  <div className ="App">
+    <ExpenseItem data={expenses[0] }  />  
+    <ExpenseItem data={expenses[1] }  />  
+
+  </div>
+);
 }
 
 export default App;
