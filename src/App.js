21bc4866e75d7652b7db81b845ad619d1a4d1317
Kaisa Ets <kaisa.ets@voco.ee>
2024-12-13 15:08:43 +0200

    Add styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 const App = () =>{
   const expenses =[
@@ -18,9 +18,7 @@ const App = () =>{
 
 return (
   <div className ="App">
-    <ExpenseItem data={expenses[0] }  />  
-    <ExpenseItem data={expenses[1] }  />  
-
+    <Expenses expenses={expenses}  />   
   </div>
 );
 }
---
+++ src/components/Expenses.js
@@ -0,0 +1,14 @@
+import ExpenseItem from "./ExpenseItem"
+import './Expenses.css' 
+
+const Expenses = (props) =>{ 
+
+return (
+    <div className="expenses">
+      <ExpenseItem data={props.expenses[0] }  />  
+      <ExpenseItem data={props.expenses[1] }  />  
+  
+    </div>
+  )
+}
+export default Expenses
