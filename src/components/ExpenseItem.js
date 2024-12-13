ae8d2215389d1e2eb3f25d90f92e25d81f22d491
Kaisa Ets <kaisa.ets@voco.ee>
2024-12-13 15:37:23 +0200

    Add wrapper styling component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
+import Card from './Card'
 
 const ExpenseItem = (props) => {
     
 
     return (
-    <div class='expense-item'>
+    <Card className='expense-item'>
         <ExpenseDate date={props.data.date} /> 
         <div className='expense-item_description'>
         <h2>{props.data.title}</h2>
         <div className = 'expense-item__price'>{props.data.price}</div>
-    </div>
-    </div>
+        </div>
+    </Card>
     )
 }
 export default ExpenseItem
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,14 +1,15 @@
 import ExpenseItem from "./ExpenseItem"
+import Card from "./Card"
 import './Expenses.css' 
 
 const Expenses = (props) =>{ 
 
 return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem data={props.expenses[0] }  />  
       <ExpenseItem data={props.expenses[1] }  />  
   
-    </div>
+    </Card>
   )
 }
 export default Expenses
