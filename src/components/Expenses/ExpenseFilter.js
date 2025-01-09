8910f0176d071e1bfd474e35ac91f7c064cc7a36
Kaisa Ets <kaisa.ets@voco.ee>
2025-01-09 13:57:17 +0200

    yl4

---
+++ src/components/Expenses/ExpenseFilter.js
@@ -0,0 +1,22 @@
+import "./ExpenseFilter.css";
+
+const ExpensesFilter = (props) => {
+  const yearChangeHandler = (event) => {
+    props.onChangeYear(event.target.value);
+  };
+
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label>Filter by year</label>
+        <select onChange={yearChangeHandler}>
+          <option value="2024">2024</option>
+          <option value="2025">2025</option>
+          <option value="2026">2026</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,18 @@
 import ExpenseItem from "./ExpenseItem"
 import Card from "../UI/Card"
 import './Expenses.css' 
+import ExpensesFilter from "./ExpenseFilter"
 
 const Expenses = (props) =>{ 
-
+ const yearChangeHandler = (year) =>{
+  console.log(year)
+ } 
+ 
 return (
     <Card className="expenses">
+      <ExpensesFilter onChangeYear = {yearChangeHandler} />
       <ExpenseItem data={props.expenses[0] }  />  
-      <ExpenseItem data={props.expenses[1] }  />  
-  
+      <ExpenseItem data={props.expenses[1] }  />
     </Card>
   )
 }
